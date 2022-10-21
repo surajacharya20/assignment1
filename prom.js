7922bc98746c1f71d5a61d7c0ab351f6112e7678
surajacharya20 <115579115+surajacharya20@users.noreply.github.com>
2022-10-21 12:17:41 +0530

    updated promises

--- prom.js
+++ prom.js
@@ -1,12 +1,10 @@
 let pro= new Promise((resolve, reject) => {
       resolve("Success");
-      reject("Failed");
      });
   
   pro.then((message) => {
     console.log(`state is resolved and ${message}`);
-  }).catch((message) => {
-    console.log(`state is rejected and ${message}`);
   });
+ 
   
   
