fa911e8f95821e9c1ab214b54e44615c58168a1d
surajacharya20 <115579115+surajacharya20@users.noreply.github.com>
2022-10-21 09:27:31 +0530

    updated prom.js

--- prom.js
+++ prom.js
@@ -1,11 +1,7 @@
 let pro= new Promise((resolve, reject) => {
-    let a = 1 * 5;
-    if (a == 5) {
       resolve("Success");
-    } else {
       reject("Failed");
-    }
-  });
+     });
   
   pro.then((message) => {
     console.log(`state is resolved and ${message}`);
